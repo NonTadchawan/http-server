c47d2556a704798d36843f7cad2e694d39e0d44c
non <tadchawan@geniustree.co.th>
2023-05-22 11:07:21 +0700

    edit class HttpServer add method start for Thread

--- src/main/java/org/example/server/HttpServer.java
+++ src/main/java/org/example/server/HttpServer.java
@@ -29,7 +29,7 @@ public class HttpServer {
                 }catch (Exception e){
                     e.printStackTrace();
                 }
-            });
+            }).start();
 
         }
     }
